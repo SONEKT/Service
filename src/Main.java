8ab69b6e55c92cab3a185d0d50f44149d0452df8
SONEKT <shulmanlevgen@gmail.com>
2023-07-14 22:13:19 +0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("car1");
+        Car car2 = new Car("car2");
+        car.service();
+
+        Truck truck = new Truck("truck1", 6);
+        Truck truck2 = new Truck("truck2", 8);
+        truck.service();
+
+        Bicycle bicycle = new Bicycle("bicycle1", 2);
+        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(car);
+        station.check(car2);
+        station.check(truck);
+        station.check(truck2);
+        station.check(bicycle);
+        station.check(bicycle2);
     }
 }
